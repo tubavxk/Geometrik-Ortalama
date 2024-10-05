8c5a28923c3786bac261b9a9d6f57baad4d42454
tubavxk <tubaazsoy@gmail.com>
2024-10-05 10:27:48 +0300

    Geometrik Ortalama
    2 Saynın geometrik ortalamasını hesaplamak için geliştirilmiştir.

---
+++ merhaba c#/Program.cs
@@ -0,0 +1,8 @@
+﻿int a, b;
+Console.WriteLine("1.Sayı");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("2.Sayı");
+b = Convert.ToInt32(Console.ReadLine());
+int c = (a + b)/2;
+Console.WriteLine("SayılarınOrtalaması=" + c);
+Console.ReadKey();
