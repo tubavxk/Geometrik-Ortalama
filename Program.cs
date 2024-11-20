c6f8ced17dad703c9099582a0bca495499ae8137
Tuba <tubaazsoy@gmail.com>
2024-11-20 23:50:43 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Birinci sayıyı girin:");
+double sayi1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("İkinci sayıyı girin:");
+double sayi2 = Convert.ToDouble(Console.ReadLine());
+
+double ortalama = (sayi1 + sayi2 / 2);
+
+Console.WriteLine(ortalama);
